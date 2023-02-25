0d61d3bb4fa3822c48e0762c99c26abe894cdff6
Sana Rayess <rayess.sana@gmail.com>
2023-02-25 23:54:34 +0200

    added merge function

---
+++ page1.js
@@ -0,0 +1,52 @@
+function sortNumbers() {
+    
+    const input = prompt("Please enter 10 numbers separated by commas:");
+  
+    // Split the input string into an array of numbers
+    const numbers = input.split(",").map(Number);
+  
+    
+    const numbersOutput = document.getElementById("numbers-output");
+    numbersOutput.textContent = numbers.join(", ");
+
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(
+      mergeSort(left),
+      mergeSort(right)
+    );
+  }
+
+  function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  }
+
+  
+  const sortedNumbers = mergeSort(numbers);
+  const sortedOutput = document.getElementById("sorted-output");
+  sortedOutput.textContent = sortedNumbers.join(", ");
+
+}
+
